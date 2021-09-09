dc99941708445fb0a6babc723ab0290d7f51a560
lrg1031@live.com <itlabs@WHB019_19.it.farmingdale.edu>
2021-09-09 14:26:03 -0400

    added one dimension array with initial values

---
+++ CSE211_WEEK2/src/main/java/com/mycompany/cse211_week2/NewClass.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.cse211_week2;
+
+
+public class NewClass {
+    
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 5;
+        arr[2] = 10;
+        for(int i=0; i<5; i++) {
+            System.out.println("Array elemtne value: " + arr[i]);
+        }
+    }
+    
+}
